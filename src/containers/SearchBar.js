d7d0fb78a1bd0aeffcb6aa8b2bd2f7c4661fbff4
Minty <Ronak>
2017-08-01 20:54:20 -0500

    Minor changes

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
@@ -13,11 +16,13 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     //TODO: Fetch weather data here using API
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
+      <form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
         <input 
           placeholder="Get a five day forecast in your favorite cities!"
           className="form-control"
@@ -30,3 +35,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-
+import WeatherReducer from './WeatherReducer';
 
 const rootReducer = combineReducers({
-
+  weather: WeatherReducer
 });
 
 export default rootReducer;
