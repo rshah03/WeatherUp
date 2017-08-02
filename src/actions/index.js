0f6085d9600c1674902f7794af12b438d6d23868
Minty <Ronak>
2017-08-01 19:16:02 -0500

    Added Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
 import config from '../../config';
 
 const API_KEY = config.MY_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER,
+    
+  };
+}
