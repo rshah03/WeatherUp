27ebf9ce7ff35eff026391d0c2ee0013d1f76cca
Minty <Ronak>
2017-08-03 02:16:47 -0500

    Mapped cities

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -3,6 +3,18 @@ import { connect } from 'react-redux';
 
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+
+    return (
+      <tr key={name}>
+        <td>
+          {name}
+        </td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -15,7 +27,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-        
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
