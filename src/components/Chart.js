31368c87c3b5b16e02598f00b3a7f790e683a514
Minty <Ronak>
2017-08-03 03:57:50 -0500

    Added lodash to calc avg values

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
 
 //<Chart dataIn={tempArr}/>
 export default (props) => {
@@ -7,7 +12,9 @@ export default (props) => {
     <div>
       <Sparklines height={120} width={180} data={props.dataIn}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.dataIn)}</div>
     </div>
   );
 };
